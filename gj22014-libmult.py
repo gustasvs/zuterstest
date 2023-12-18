0ae5cc5a88a52df217d8dac507d05465047f7867
gustavs@dsolutions.lv <gustavs@dsolutions.lv>
2023-12-18 14:40:00 +0200

    student id in names

--- gj22014-libmult.py
+++ gj22014-libmult.py
@@ -1,4 +1,5 @@
 def mult(a,b):
     return a*b
-def multx(a,b,c):
+def multx(a,b,c): 
     return a*b*c
+ 
--- gj22014-main.py
+++ gj22014-main.py
@@ -2,7 +2,7 @@
 # created to demonstrate version control.
 from libadd import *
 from libmult import *
-a = 13
+a = 13 
 b = 5
 print(plus(a,b))
 print(mult(a,b))
